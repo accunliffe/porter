c85fdcd73ba2cd50a2539b42816dfbc1774d37c5
Andrew Cunliffe <rhinocapitalmgmt@gmail.com>
2015-03-05 15:02:38 -0800

    loading dependencies in package.json

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('port', ['ngRoute', 'ngAnimate', 'ui.bootstrap']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/about', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    $routeProvider.when('/home', {templateUrl: 'partials/home', controller: MyCtrl1});
+    $routeProvider.when('/about', {templateUrl: 'partials/about', controller: MyCtrl2});
     $routeProvider.when('/view3', {templateUrl: 'partials/partial3', controller: MyCtrl3});
     $routeProvider.when('/view4', {templateUrl: 'partials/partial4', controller: MyCtrl4});
     $routeProvider.when('myModalContent', {templateUrl: 'partials/myModalContent', controller: ModalCtrl})
